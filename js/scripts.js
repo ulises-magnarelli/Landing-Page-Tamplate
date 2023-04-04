dadf7fb8486e8ca70d5a68542e5fdfaed925d2bd
Ulises Octavio Magnarelli <uli.magnarelli@gmail.com>
2023-04-04 10:43:58 -0300

    Uploading functional landing page template

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+//shows responsive menu
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className ===""){
+        x.className = "responsive"
+    }else {
+        x.className = "";
+    }
+}
